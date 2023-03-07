9f083bd5134ac4b48f427134c8eb4f7667202b70
mishraom12345 <mishrahariom525@gmail.com>
2023-03-07 08:16:06 +0530

    ceating and Giving functionality to login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignUP from './Component/pages/SignUp';
+import Login from './Component/pages/Login';
+import Welcome from './Component/pages/Welcome';
 
 function App() {
   return (
     
     <div>
       <Routes>
-      <Route path = '/' element= {<SignUP/>}/>
+      <Route path='/' element = {<Login/>}/>
+      <Route path = '/signup' element= {<SignUP/>}/>
+      <Route path = '/welcome' element = {<Welcome/>}/>
+      
      </Routes>
     </div>
    
--- src/store/AuthSlice.js
+++ src/store/AuthSlice.js
@@ -2,5 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 import React from "react";
 
 const initialstate = {
-    
+    isLogin:false,
+    idToken : localStorage.getItem('idToken')
 }
+
+const authslice = createSlice({
+    name:'authentication',
+    initialState:initialstate,
+    reducers:{
+        LogIn(state,action){
+            localStorage.setItem('idToken',action.payload)
+            state.isLogin = true
+            state.idToken = action.payload
+        }
+    }
+
+})
+
+export const authAction = authslice.actions
+export default authslice
