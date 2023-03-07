72b0b985414ee3bd5ca83205d55fc5973b559ab2
mishraom12345 <mishrahariom525@gmail.com>
2023-03-07 11:47:27 +0530

    making sentbox&&  Compose and send Mail

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import SignUP from './Component/pages/SignUp';
 import Login from './Component/pages/Login';
 import Welcome from './Component/pages/Welcome';
+import Header from './Component/Header/Header';
+import Sentbox from './Component/pages/Sentbox';
 
 function App() {
   return (
     
     <div>
+      <Header/>
       <Routes>
       <Route path='/' element = {<Login/>}/>
       <Route path = '/signup' element= {<SignUP/>}/>
       <Route path = '/welcome' element = {<Welcome/>}/>
+      <Route path='/sentbox' element = {<Sentbox/>} />
       
      </Routes>
     </div>
--- src/Component/pages/Welcome.js
+++ src/Component/pages/Welcome.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import EmailForm from './EmailForm'
+import Sentbox from './Sentbox'
 
 function Welcome() {
   return (
-    <div>
+   <div>
+     <div style={{textAlign:'center'}}>
+
       <h2>welcome to mail box client</h2>
     </div>
+    <hr/>
+    <EmailForm/>
+   </div>
+    
   )
 }
 
